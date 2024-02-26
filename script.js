65cbe5e6e1ab2157e48d14e3cc674da62329e6a5
Yuki2no <eragbonikpeya@gmail.com>
2024-02-26 10:36:37 +0100

    output values can now be seen
    modified output and input text layout

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
 const answer = document.getElementById('answer');
+const numValue = document.getElementById('value');
+let result;
 
 let currentNum = '';
 let previousNum = '';
 let operators = '';
 function number(num){
    currentNum += num;
-   console.log(currentNum);
+   numValue.textContent = currentNum;
 }
 
 function operator(op){
     operators = op;
-    console.log(operators)
+    numValue.textContent = operators;
     previousNum = currentNum;
     currentNum = '';
 }
 
+function calculate(){
+    const num1 = parseFloat(previousNum);
+    const num2 = parseFloat(currentNum);
+
+    switch(operators){
+        case '+':
+            result = num1 + num2;
+            answer.textContent = result;
+            break;
+        
+        case '-':
+         result = num1 - num2;
+         answer.textContent = result;
+         break;
+         
+        case 'x':
+            result = num1 * num2;
+            answer.textContent = result;
+            break;
+
+        case '/':
+            result = num1 / num2;
+            if(num2 === 0){
+                answer.textContent = 'MATH ERROR';
+                break;
+            } else{
+                answer.textContent = result;
+                break;
+            }
+    }
+
+}
+
+function clear(){
+    currentNum = '';
+    previousNum = '';
+    operators = '';
+    answer.textContent = "";
+    numValue.textContent = "";
+}
